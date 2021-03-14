70f3c97cfbb7d3441f21fb110db481a33295b5a0
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-14 12:25:23 +0200

    Initial Commit

---
+++ Shared/FetchingJSONDataFromAnAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchingJSONDataFromAnAPIApp.swift
+//  Shared
+//
+//  Created by Ramill Ibragimov on 14.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct FetchingJSONDataFromAnAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
